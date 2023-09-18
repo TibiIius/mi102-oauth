03fed63980242a20da7b61bf688c2e13ba7a974e
TimFB <oss@timfb.dev>
2023-09-18 19:04:10 +0200

    Update deps and prepare sessions

--- confidential-client/nuxt.config.ts
+++ confidential-client/nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module"],
+  devServer: {
+    port: 3050,
+  },
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@sidebase/nuxt-session",
+    "@nuxtjs/eslint-module",
+  ],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  session: {
+    session: {
+      cookieSecure: true,
+      cookieHttpOnly: true,
     },
   },
 });
